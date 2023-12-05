3731296b56bc7e9faceaf00c8e289ab5c49917f5
Mukhamed Karanashev <mukhamed.karanashev@infobip.com>
2023-12-05 11:23:32 +0100

    Day 5 part 2: naive solution

--- src/day5.rs
+++ src/day5.rs
@@ -2,28 +2,42 @@ use std::fmt::Display;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::time::Instant;
 
 pub fn part1(input_path: &Path) {
     let almanac = load_input(input_path).unwrap();
     eprintln!("Loaded almanac:");
-    eprintln!("Seeds: {:?}", &almanac.seeds);
-    eprintln!("Rules:");
-    for (i, rules) in almanac.maps.iter().enumerate() {
-        eprintln!("Map {}", i);
-        for rr in rules {
-            eprintln!("{}", rr);
-        }
-    }
+    eprintln!("{}", &almanac);
     let result = almanac
         .seeds
         .iter()
-        .map(|seed| map_to_location(*seed, &almanac.maps))
+        .map(|seed| map_to_location(*seed, &almanac.rule_book))
         .min();
     println!("Result: {}", result.unwrap())
 }
 
 pub fn part2(input_path: &Path) {
-    todo!()
+    let start = Instant::now();
+    let almanac = load_input(input_path).unwrap();
+    eprintln!("Loaded almanac:");
+    eprintln!("{}", &almanac);
+    let result = almanac
+        .seeds
+        .chunks(2)
+        .map(|c| c[0]..(c[0] + c[1]))
+        .enumerate()
+        .map(|(i, seed_range)| {
+            eprintln!("Start seed range {} of {}: start={}, length={}", 
+                      i, almanac.seeds.len() / 2, &seed_range.start, &seed_range.end - &seed_range.start);
+            eprintln!("Total running time before: {:?}", start.elapsed());
+            seed_range
+                .map(|seed| map_to_location(seed, &almanac.rule_book))
+                .min()
+                .unwrap()
+        })
+        .min();
+
+    println!("Result: {}", result.unwrap())
 }
 
 struct RangeRule {
@@ -32,15 +46,29 @@ struct RangeRule {
     range_len: u64,
 }
 
+struct Almanac {
+    seeds: Vec<u64>,
+    rule_book: Vec<Vec<RangeRule>>,
+}
+
 impl Display for RangeRule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {} {}", self.dst_key, self.src_key, self.range_len)
     }
 }
 
-struct Almanac {
-    seeds: Vec<u64>,
-    maps: Vec<Vec<RangeRule>>,
+impl Display for Almanac {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "seeds: {:?}\n", self.seeds)?;
+        for (idx, rules_map) in self.rule_book.iter().enumerate() {
+            writeln!(f, "map {}:", idx)?;
+            for rr in rules_map {
+                writeln!(f, "{}", rr)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 fn map_key(key: u64, rule_map: &Vec<RangeRule>) -> u64 {
@@ -50,8 +78,8 @@ fn map_key(key: u64, rule_map: &Vec<RangeRule>) -> u64 {
         .iter()
         .find(|r| (r.src_key..(r.src_key + r.range_len)).contains(&key))
         .map(|rr| {
-            eprintln!("key: {}, map rule: {}", key, rr);
-            eprint!("{} + {} - {} = ", rr.dst_key, key, rr.src_key);
+            // eprintln!("key: {}, map rule: {}", key, rr);
+            // eprint!("{} + {} - {} = ", rr.dst_key, key, rr.src_key);
             rr.dst_key + key - rr.src_key
         })
         .unwrap_or(key)
@@ -59,9 +87,9 @@ fn map_key(key: u64, rule_map: &Vec<RangeRule>) -> u64 {
 
 fn map_to_location(seed: u64, rule_book: &Vec<Vec<RangeRule>>) -> u64 {
     rule_book.iter().fold(seed, |key, rules| {
-        eprintln!("Check key {}", key);
+        // eprintln!("Check key {}", key);
         let result = map_key(key, rules);
-        eprintln!("Key mapped to next key: {}", result);
+        // eprintln!("Key mapped to next key: {}", result);
         result
     })
 }
@@ -101,5 +129,8 @@ fn load_input(input_path: &Path) -> io::Result<Almanac> {
         .split(|line| line.is_empty())
         .map(|lines| parse_rules(lines))
         .collect();
-    return Ok(Almanac { seeds, maps });
+    return Ok(Almanac {
+        seeds,
+        rule_book: maps,
+    });
 }
