c9ed066b20dc0aeba4ab49f9fa2005d68945d261
Mukhamed Karanashev <menchauser@gmail.com>
2023-12-08 08:04:01 +0100

    Update template for future

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn part1(input_path: &Path) {
@@ -6,5 +9,12 @@ pub fn part1(input_path: &Path) {
 
 #[allow(unused)]
 pub fn part2(input_path: &Path) {
+    todo!()
+}
+
+fn load_input(input_path: &Path) -> HashMap<&str, (&str, &str)> {
+    let file = File::open(input_path)?;
+    let buf_reader = io::BufReader::new(file);
+    let mut lines = buf_reader.lines();
     todo!()
 }
--- src/template.rs
+++ src/template.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn part1(input_path: &Path) {
@@ -6,5 +8,12 @@ pub fn part1(input_path: &Path) {
 
 #[allow(unused)]
 pub fn part2(input_path: &Path) {
+    todo!()
+}
+
+fn load_input(input_path: &Path) -> io::Result<_> {
+    let file = File::open(input_path)?;
+    let buf_reader = io::BufReader::new(file);
+    let mut lines = buf_reader.lines();
     todo!()
 }
