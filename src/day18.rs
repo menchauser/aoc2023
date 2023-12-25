5aa9b96f7d69df5c25f2f45f4b30e372d0f59f9f
Mukhamed Karanashev <mukhamed.karanashev@infobip.com>
2023-12-25 17:54:04 +0100

    Prepare for Day 18 Part 2

--- src/day18.rs
+++ src/day18.rs
@@ -1,125 +1,13 @@
 use std::collections::VecDeque;
+use std::fmt::Display;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn part1(input_path: &Path) {
-    let dig_plan = load_input(input_path).unwrap();
+    let dig_plan = load_input(input_path, parse_instruction_1).unwrap();
+    let mut plan = draw_plan(&dig_plan);
     // now the naive approach is to build a 'picture' and then fill it horizontally
-    let mut plan: Vec<Vec<char>> = vec![vec!['.']];
-    let mut row_idx: usize = 0;
-    let mut col_idx: usize = 0;
-    for step in dig_plan {
-        match step.dir {
-            Direction::Up => {
-                // we should insert (step.meters - row) rows up
-                // eprintln!(
-                //     "Try going up {} meters: row={}, total rows={}",
-                //     step.meters,
-                //     row_idx,
-                //     plan.len()
-                // );
-                let needed_rows = step.meters as i16 - row_idx as i16;
-                let needed_cols = plan[row_idx].len();
-                if needed_rows > 0 {
-                    plan.reserve(needed_rows as usize);
-                    for _ in 0..needed_rows {
-                        plan.insert(0, vec!['.'; needed_cols]);
-                    }
-                    // for every inserted row our current position is shifted forward
-                    row_idx += needed_rows as usize;
-                }
-                // insert '#' chars
-                // eprintln!("row_idx: {}", row_idx);
-                // eprintln!("needed_ros: {}", needed_rows as usize);
-                // eprintln!("meters: {}", step.meters);
-                for i in (row_idx - step.meters)..row_idx {
-                    plan[i][col_idx] = '#';
-                }
-                // row should move from current row meters back
-                row_idx = row_idx - step.meters;
-            }
-            Direction::Down => {
-                // we should insert (step.meters - (len - row - 1)) rows down
-                // eprintln!(
-                //     "Try going down {} meters: row={}, total rows={}",
-                //     step.meters,
-                //     row_idx,
-                //     plan.len()
-                // );
-                let needed_rows = step.meters as i16 - (plan.len() - row_idx - 1) as i16;
-                let needed_cols = plan[row_idx].len();
-                if needed_rows > 0 {
-                    plan.reserve(needed_rows as usize);
-                    for _ in 0..needed_rows {
-                        plan.push(vec!['.'; needed_cols])
-                    }
-                }
-                // insert '#' chars
-                for i in row_idx..(row_idx + step.meters + 1) {
-                    plan[i][col_idx] = '#';
-                }
-                row_idx = row_idx + step.meters;
-            }
-            Direction::Left => {
-                // we should insert (step.meters - col) cols left
-                // eprintln!(
-                //     "Try going left {} meters: col={}, total cols={}",
-                //     step.meters,
-                //     col_idx,
-                //     plan[row_idx].len()
-                // );
-                let needed_cols = step.meters as i16 - col_idx as i16;
-                if needed_cols > 0 {
-                    for _ in 0..needed_cols {
-                        for row in plan.iter_mut() {
-                            row.reserve(needed_cols as usize);
-                            row.insert(0, '.');
-                        }
-                    }
-                    // for every inserted col our current position is shifted right
-                    col_idx += needed_cols as usize;
-                }
-                // insert '#' chars
-                for j in (col_idx - step.meters)..col_idx {
-                    plan[row_idx][j] = '#';
-                }
-                // col should move to the left
-                col_idx = col_idx - step.meters;
-            }
-            Direction::Right => {
-                // we should insert (step.meters - (len - col - 1)) cols right
-                // eprintln!(
-                //     "Try going right {} meters: col={}, total cols={}",
-                //     step.meters,
-                //     col_idx,
-                //     plan[row_idx].len()
-                // );
-                // let needed_rows = plan.len();
-                let needed_cols = step.meters as i16 - (plan[row_idx].len() - col_idx - 1) as i16;
-                if needed_cols > 0 {
-                    for _ in 0..needed_cols {
-                        for row in plan.iter_mut() {
-                            row.reserve(needed_cols as usize);
-                            row.push('.');
-                        }
-                    }
-                }
-                // insert '#' chars
-                for j in col_idx..(col_idx + step.meters) {
-                    plan[row_idx][j] = '#';
-                }
-                col_idx = col_idx + step.meters;
-            }
-        }
-    }
-    // now second part: fill field
-    // to do that we insert one more col to the left and start filling
-    // for row in plan.iter_mut() {
-    //     // row.insert(0, '.');
-    //     row.push('.');
-    // }
-    // plan.insert(0, vec!['.'; plan[0].len()]);
     eprintln!("Result field");
     for (i, row) in plan.iter().enumerate() {
         print!("{:04}  ", i);
@@ -159,6 +47,147 @@ pub fn part1(input_path: &Path) {
         .map(|row| row.iter().filter(|c| **c == '#').count())
         .sum();
     println!("Result: {}", result);
+}
+
+pub fn part2(input_path: &Path) {
+    let dig_plan = load_input(input_path, parse_instruction_2).unwrap();
+    eprintln!("Plan:");
+    for ds in &dig_plan {
+        eprintln!("{}", ds);
+    }
+    let mut plan = draw_plan(&dig_plan);
+    eprintln!("Result field");
+    for (i, row) in plan.iter().enumerate() {
+        print!("{:06}  ", i);
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
+    }
+    // now let's find first 'inner' position and try to fill from it
+    let start_point = find_inner_point(&plan);
+    let mut next_points = VecDeque::from([start_point]);
+    println!("Found inside point: {:?}", start_point);
+    while !next_points.is_empty() {
+        let (i, j) = next_points.pop_front().unwrap();
+        plan[i][j] = '#';
+        // now let's try going in 4 directions
+        let directions = [(-1i32, 0i32), (0, 1), (1, 0), (0, -1)];
+        for (di, dj) in directions {
+            let new_i = (i as i32 + di) as usize;
+            let new_j = (j as i32 + dj) as usize;
+            if plan[new_i][new_j] == '.' {
+                plan[new_i][new_j] = '#';
+                next_points.push_back((new_i, new_j));
+            }
+        }
+    }
+    println!("Filled plan:");
+    for (i, row) in plan.iter().enumerate() {
+        print!("{:06}  ", i);
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
+    }
+    let result: usize = plan
+        .iter()
+        .map(|row| row.iter().filter(|c| **c == '#').count())
+        .sum();
+    println!("Result: {}", result);
+}
+
+// Take list of instructions and return drawn map
+fn draw_plan(instructions: &Vec<DigStep>) -> Vec<Vec<char>> {
+    let mut plan: Vec<Vec<char>> = vec![vec!['.']];
+    let mut row_idx: usize = 0;
+    let mut col_idx: usize = 0;
+    for step in instructions {
+        eprintln!(
+            "Processing step: {}, row_idx={}, col_idx={}",
+            step, row_idx, col_idx
+        );
+        match step.dir {
+            Direction::Up => {
+                // we should insert (step.meters - row) rows up
+                let needed_rows = step.meters as i32 - row_idx as i32;
+                let needed_cols = plan[row_idx].len();
+                if needed_rows > 0 {
+                    plan.reserve(needed_rows as usize);
+                    for _ in 0..needed_rows {
+                        plan.insert(0, vec!['.'; needed_cols]);
+                    }
+                    // for every inserted row our current position is shifted forward
+                    row_idx += needed_rows as usize;
+                }
+                for i in (row_idx - step.meters)..row_idx {
+                    plan[i][col_idx] = '#';
+                }
+                // row should move from current row meters back
+                row_idx = row_idx - step.meters;
+            }
+            Direction::Down => {
+                // we should insert (step.meters - (len - row - 1)) rows down
+                let needed_rows = step.meters as i32 - (plan.len() - row_idx - 1) as i32;
+                let needed_cols = plan[row_idx].len();
+                if needed_rows > 0 {
+                    eprintln!("D: Reserving {} additional rows...", needed_rows);
+                    plan.reserve(needed_rows as usize);
+                    eprintln!("D: Reserved. Adding row values...");
+                    let new_row = vec!['.'; needed_cols];
+                    for i in 0..needed_rows {
+                        plan.push(new_row.clone());
+                    }
+                    eprintln!("D: Added");
+                }
+                eprintln!("D: Filling down chars with '#':");
+                // insert '#' chars
+                for i in row_idx..(row_idx + step.meters + 1) {
+                    plan[i][col_idx] = '#';
+                }
+                eprintln!("D: Filled");
+                row_idx = row_idx + step.meters;
+            }
+            Direction::Left => {
+                // we should insert (step.meters - col) cols left
+                let needed_cols = step.meters as i32 - col_idx as i32;
+                if needed_cols > 0 {
+                    for _ in 0..needed_cols {
+                        for row in plan.iter_mut() {
+                            row.reserve(needed_cols as usize);
+                            row.insert(0, '.');
+                        }
+                    }
+                    // for every inserted col our current position is shifted right
+                    col_idx += needed_cols as usize;
+                }
+                // insert '#' chars
+                for j in (col_idx - step.meters)..col_idx {
+                    plan[row_idx][j] = '#';
+                }
+                // col should move to the left
+                col_idx = col_idx - step.meters;
+            }
+            Direction::Right => {
+                // we should insert (step.meters - (len - col - 1)) cols right
+                let needed_cols = step.meters as i32 - (plan[row_idx].len() - col_idx - 1) as i32;
+                if needed_cols > 0 {
+                    for _ in 0..needed_cols {
+                        for row in plan.iter_mut() {
+                            row.reserve(needed_cols as usize);
+                            row.push('.');
+                        }
+                    }
+                }
+                // insert '#' chars
+                for j in col_idx..(col_idx + step.meters) {
+                    plan[row_idx][j] = '#';
+                }
+                col_idx = col_idx + step.meters;
+            }
+        }
+    }
+    plan
 }
 
 // Find first point inside given plan
@@ -267,12 +296,7 @@ fn scan_fill(plan: &Vec<Vec<char>>) {
     println!("Result: {}", result);
 }
 
-#[allow(unused)]
-pub fn part2(input_path: &Path) {
-    todo!()
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -280,11 +304,22 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct DigStep {
     dir: Direction,
     meters: usize,
-    // TODO: rgb
+}
+
+impl Display for DigStep {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let dir_s = match self.dir {
+            Direction::Up => 'U',
+            Direction::Down => 'D',
+            Direction::Left => 'L',
+            Direction::Right => 'R',
+        };
+        write!(f, "{} {}", dir_s, self.meters)
+    }
 }
 
 fn parse_dir(s: &str) -> Direction {
@@ -297,20 +332,61 @@ fn parse_dir(s: &str) -> Direction {
     }
 }
 
-fn load_input(input_path: &Path) -> io::Result<Vec<DigStep>> {
+fn parse_instruction_1(line: &str) -> DigStep {
+    DigStep {
+        dir: parse_dir(&line[0..1]),
+        meters: line
+            .split_whitespace()
+            .nth(1)
+            .and_then(|s| s.parse().ok())
+            .unwrap(),
+    }
+}
+
+fn parse_instruction_2(line: &str) -> DigStep {
+    let hex_s = &line[line.find('#').unwrap() + 1..(line.len() - 1)];
+    let dir_n: u8 = hex_s[5..6].parse().unwrap();
+    let dir = match dir_n {
+        0 => Direction::Right,
+        1 => Direction::Down,
+        2 => Direction::Left,
+        3 => Direction::Up,
+        _ => unimplemented!(),
+    };
+    DigStep {
+        dir: dir,
+        meters: usize::from_str_radix(&hex_s[0..5], 16).unwrap(),
+    }
+}
+
+fn load_input(input_path: &Path, parse_i: fn(&str) -> DigStep) -> io::Result<Vec<DigStep>> {
     let file = File::open(input_path)?;
     let buf_reader = io::BufReader::new(file);
     buf_reader
         .lines()
-        .map(|line| {
-            line.map(|l| DigStep {
-                dir: parse_dir(&l[0..1]),
-                meters: l
-                    .split_whitespace()
-                    .nth(1)
-                    .and_then(|s| s.parse().ok())
-                    .unwrap(),
-            })
-        })
+        .map(|line| line.map(|l| parse_i(&l)))
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_part_2() {
+        assert_eq!(
+            DigStep {
+                dir: Direction::Right,
+                meters: 461937
+            },
+            parse_instruction_2("R 6 (#70c710)")
+        );
+        assert_eq!(
+            DigStep {
+                dir: Direction::Down,
+                meters: 56407
+            },
+            parse_instruction_2("D 5 (#0dc571)")
+        );
+    }
 }
