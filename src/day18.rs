ad7e9550560516c03d2da44fa952e9348a36868f
Mukhamed Karanashev <mukhamed.karanashev@infobip.com>
2023-12-25 18:57:04 +0100

    WIP: Day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+use num::iter;
+
 pub fn part1(input_path: &Path) {
     let dig_plan = load_input(input_path, parse_instruction_1).unwrap();
     let mut plan = draw_plan(&dig_plan);
@@ -50,51 +52,60 @@ pub fn part1(input_path: &Path) {
 }
 
 pub fn part2(input_path: &Path) {
-    let dig_plan = load_input(input_path, parse_instruction_2).unwrap();
+    let dig_plan = load_input(input_path, parse_instruction_1).unwrap();
     eprintln!("Plan:");
     for ds in &dig_plan {
         eprintln!("{}", ds);
     }
-    let mut plan = draw_plan(&dig_plan);
-    eprintln!("Result field");
-    for (i, row) in plan.iter().enumerate() {
-        print!("{:06}  ", i);
-        for c in row {
-            print!("{}", c);
-        }
-        println!();
+    eprintln!("Estimage plan size as keys of keys");
+    let result: usize = dig_plan.iter().map(|ds| ds.meters).sum();
+    eprintln!("{} pixels map to {} bytes", result, result * 24);
+    let plan = draw_plan_sparse(&dig_plan);
+    for row in &plan {
+        println!("{:?}", row)
     }
+    print_plan_sparse(&plan);
+
+    // let mut plan = draw_plan(&dig_plan);
+    // eprintln!("Result field");
+    // for (i, row) in plan.iter().enumerate() {
+    //     print!("{:06}  ", i);
+    //     for c in row {
+    //         print!("{}", c);
+    //     }
+    //     println!();
+    // }
     // now let's find first 'inner' position and try to fill from it
-    let start_point = find_inner_point(&plan);
-    let mut next_points = VecDeque::from([start_point]);
-    println!("Found inside point: {:?}", start_point);
-    while !next_points.is_empty() {
-        let (i, j) = next_points.pop_front().unwrap();
-        plan[i][j] = '#';
-        // now let's try going in 4 directions
-        let directions = [(-1i32, 0i32), (0, 1), (1, 0), (0, -1)];
-        for (di, dj) in directions {
-            let new_i = (i as i32 + di) as usize;
-            let new_j = (j as i32 + dj) as usize;
-            if plan[new_i][new_j] == '.' {
-                plan[new_i][new_j] = '#';
-                next_points.push_back((new_i, new_j));
-            }
-        }
-    }
-    println!("Filled plan:");
-    for (i, row) in plan.iter().enumerate() {
-        print!("{:06}  ", i);
-        for c in row {
-            print!("{}", c);
-        }
-        println!();
-    }
-    let result: usize = plan
-        .iter()
-        .map(|row| row.iter().filter(|c| **c == '#').count())
-        .sum();
-    println!("Result: {}", result);
+    // let start_point = find_inner_point(&plan);
+    // let mut next_points = VecDeque::from([start_point]);
+    // println!("Found inside point: {:?}", start_point);
+    // while !next_points.is_empty() {
+    //     let (i, j) = next_points.pop_front().unwrap();
+    //     plan[i][j] = '#';
+    //     // now let's try going in 4 directions
+    //     let directions = [(-1i32, 0i32), (0, 1), (1, 0), (0, -1)];
+    //     for (di, dj) in directions {
+    //         let new_i = (i as i32 + di) as usize;
+    //         let new_j = (j as i32 + dj) as usize;
+    //         if plan[new_i][new_j] == '.' {
+    //             plan[new_i][new_j] = '#';
+    //             next_points.push_back((new_i, new_j));
+    //         }
+    //     }
+    // }
+    // println!("Filled plan:");
+    // for (i, row) in plan.iter().enumerate() {
+    //     print!("{:06}  ", i);
+    //     for c in row {
+    //         print!("{}", c);
+    //     }
+    //     println!();
+    // }
+    // let result: usize = plan
+    //     .iter()
+    //     .map(|row| row.iter().filter(|c| **c == '#').count())
+    //     .sum();
+    // println!("Result: {}", result);
 }
 
 // Take list of instructions and return drawn map
@@ -188,6 +199,125 @@ fn draw_plan(instructions: &Vec<DigStep>) -> Vec<Vec<char>> {
         }
     }
     plan
+}
+
+// Return list of lists: each row is represented by a list, each list contains (column, character)
+fn draw_plan_sparse(instructions: &Vec<DigStep>) -> Vec<Vec<(usize, char)>> {
+    let mut plan: Vec<Vec<(usize, char)>> = vec![vec![]];
+    let mut row_idx: usize = 0;
+    let mut col_idx: usize = 0;
+    let mut max_height = 1;
+    let mut max_width = 1;
+    for step in instructions {
+        eprintln!(
+            "Processing step: {}, row_idx={}, col_idx={}",
+            step, row_idx, col_idx
+        );
+        match step.dir {
+            Direction::Up => {
+                // we should insert (step.meters - row) rows up
+                let needed_rows = step.meters as i32 - row_idx as i32;
+                if needed_rows > 0 {
+                    plan.reserve(needed_rows as usize);
+                    for _ in 0..needed_rows {
+                        // we need to insert 'empty' row before
+                        plan.insert(0, vec![]);
+                    }
+                    // for every inserted row our current position is shifted forward
+                    row_idx += needed_rows as usize;
+                    max_height += needed_rows as usize;
+                }
+                // now insert an element in every new row
+                for i in (row_idx - step.meters)..row_idx {
+                    plan[i].push((col_idx, '#'));
+                }
+                // row should move from current row meters back
+                row_idx = row_idx - step.meters;
+            }
+            Direction::Down => {
+                // we should insert (step.meters - (len - row - 1)) rows down
+                let needed_rows = step.meters as i32 - (max_height - row_idx - 1) as i32;
+                if needed_rows > 0 {
+                    plan.reserve(needed_rows as usize);
+                    for _ in 0..needed_rows {
+                        plan.push(vec![]);
+                    }
+                    max_height += needed_rows as usize;
+                }
+                // insert '#' chars
+                for i in row_idx..(row_idx + step.meters + 1) {
+                    plan[i].push((col_idx, '#'));
+                }
+                row_idx = row_idx + step.meters;
+            }
+            Direction::Left => {
+                // we should insert (step.meters - col) cols left
+                let needed_cols = step.meters as i32 - col_idx as i32;
+                if needed_cols > 0 {
+                    // for _ in 0..needed_cols {
+                    //     for row in plan.iter_mut() {
+                    //         row.reserve(needed_cols as usize);
+                    //     }
+                    // }
+                    // for every inserted col our current position is shifted right
+                    plan[row_idx].reserve(needed_cols as usize);
+                    col_idx += needed_cols as usize;
+                    max_width += needed_cols as usize;
+                }
+                // insert '#' chars
+                for j in (col_idx - step.meters)..col_idx {
+                    plan[row_idx].push((j, '#'));
+                }
+                // col should move to the left
+                col_idx = col_idx - step.meters;
+            }
+            Direction::Right => {
+                // we should insert (step.meters - (len - col - 1)) cols right
+                let needed_cols = step.meters as i32 - (max_width - col_idx - 1) as i32;
+                if needed_cols > 0 {
+                    // for _ in 0..needed_cols {
+                    //     for row in plan.iter_mut() {
+                    //         row.reserve(needed_cols as usize);
+                    //     }
+                    // }
+                    plan[row_idx].reserve(needed_cols as usize);
+                    max_width += needed_cols as usize;
+                }
+                // insert '#' chars
+                for j in col_idx..(col_idx + step.meters) {
+                    plan[row_idx].push((j, '#'));
+                }
+                col_idx = col_idx + step.meters;
+            }
+        }
+    }
+    // now let's sort each row by col position
+    for row in plan.iter_mut() {
+        row.sort_by_key(|(col, _)| *col);
+    }
+    plan
+}
+
+fn print_plan_sparse(sparse_plan: &Vec<Vec<(usize, char)>>) {
+    let max_width = sparse_plan
+        .iter()
+        .map(|row| row.last().map(|(col, _)| col).unwrap())
+        .max()
+        .unwrap();
+    for row in sparse_plan {
+        let mut cur_col = 0;
+        for (col, c) in row {
+            if *col > cur_col {
+                print!("{}", ".".repeat(*col - cur_col));
+            }
+            print!("{}", c);
+            cur_col = col + 1;
+        }
+        if cur_col < *max_width {
+            print!("{}", ".".repeat(max_width - cur_col));
+        }
+        println!();
+    }
 }
 
 // Find first point inside given plan
